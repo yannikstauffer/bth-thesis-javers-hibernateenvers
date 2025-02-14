b105aa0d4e77a1245639931a3aee48b5b2701514
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2025-02-14 14:24:25 +0100

    fix: rest api

--- src/main/java/ch/ffhs/fs2025/bth_thesis_javers_hibernateenvers/rest/AbstractController.java
+++ src/main/java/ch/ffhs/fs2025/bth_thesis_javers_hibernateenvers/rest/AbstractController.java
@@ -7,15 +7,15 @@ import ch.ffhs.fs2025.bth_thesis_javers_hibernateenvers.factory.PayloadType;
 import ch.ffhs.fs2025.bth_thesis_javers_hibernateenvers.factory.TestDataFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequiredArgsConstructor
 public abstract class AbstractController<T extends BaseEntity & Thread<?>> {
@@ -31,9 +31,10 @@ public abstract class AbstractController<T extends BaseEntity & Thread<?>> {
 
     @PostMapping("/threads")
     @ResponseBody
-    public T createThread() {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer createThread() {
         T thread = testDataFactory.create(getThreadClass(), PayloadType.BASIC, ObjectGraphComplexity.SINGLE);
-        return threadRepository.save(thread);
+        return threadRepository.save(thread).getId();
     }
 
     @GetMapping("/threads")
@@ -44,8 +45,8 @@ public abstract class AbstractController<T extends BaseEntity & Thread<?>> {
 
     @GetMapping("/threads/{id}")
     @ResponseBody
-    public T getThread(@PathVariable Integer id) {
-        return threadRepository.findById(id).orElse(null);
+    public ResponseEntity<T> getThread(@PathVariable Integer id) {
+        return ResponseEntity.of(threadRepository.findById(id));
     }
 
     @DeleteMapping("/threads")
@@ -54,21 +55,20 @@ public abstract class AbstractController<T extends BaseEntity & Thread<?>> {
     }
 
     @DeleteMapping("/threads/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteThread(@PathVariable Integer id) {
         threadRepository.deleteById(id);
     }
 
-    @PatchMapping("/threads/{id}")
+    @PostMapping("/threads/{id}") // PatchMapping does not work for some weird reason
     @ResponseBody
-    @Transactional
     public ResponseEntity<T> updateThread(@PathVariable Integer id,
                                           @RequestBody ThreadDto threadDto) {
-        T thread = threadRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Thread with id " + id + "not found"));
-
-        T updated = threadRepository.save(threadDto.update(thread));
-
-        return ResponseEntity.ok(updated);
+        return ResponseEntity.of(
+                threadRepository.findById(id)
+                        .map(threadDto::update)
+                        .map(threadRepository::save)
+        );
     }
 
 }

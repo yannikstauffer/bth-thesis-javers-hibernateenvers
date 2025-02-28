c20c3c61714ba14cf9a660d731316b2713e4a247
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2025-02-28 16:31:01 +0100

    chore: update benchmark shell script

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Define an array of benchmark classes
+# Benchmarks to run
 benchmark_classes=(
   "EnversCreateBenchmark"
   "JaversCreateBenchmark"
   "NoversCreateBenchmark"
 )
 
-# Loop through each benchmark class and execute it with Maven
 for benchmark_class in "${benchmark_classes[@]}"; do
   echo "Running benchmark: $benchmark_class"
   mvn test -Dtest=$benchmark_class

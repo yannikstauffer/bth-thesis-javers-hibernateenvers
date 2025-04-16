ce2f98dd6d7c5171044bd3237c397b257ecbe990
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2025-04-16 16:17:51 +0200

    test: fix repository test

--- src/test/java/ch/ffhs/fs2025/bth_thesis_javers_hibernateenvers/novers/repository/NoversThreadRepositoryTest.java
+++ src/test/java/ch/ffhs/fs2025/bth_thesis_javers_hibernateenvers/novers/repository/NoversThreadRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ComponentScan(basePackageClasses = {DataFactory.class})
+@ActiveProfiles("h2")
 class NoversThreadRepositoryTest {
 
     @Autowired
